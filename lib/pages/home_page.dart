3422969001a6ade205ae6031700e367330a24e20
Ashraf Shah <ashrafashah@hotmail.com>
2021-05-30 10:16:08 -0600

    Day 12 - ListView completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 // ignore: must_be_immutable
 class HomePage extends StatelessWidget {
   int days = 30;
-    String name = "Codepur";
+  String name = "Codepur";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => Catalog.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-        body: Center(
-          child: Container(
-            child: Text("Welcome to $days days of flutter by $name"),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummyList[index]);
+          },
         ),
-        drawer: MyDrawer(),
-      );
+      ),
+      drawer: MyDrawer(),
+    );
   }
 }
